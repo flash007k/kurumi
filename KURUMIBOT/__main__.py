b8a111daa17170bb065bdd29ae038c8699d290f7
flash007k <80569142+flash007k@users.noreply.github.com>
2021-12-19 03:00:50 -0800

    Update __main__.py

--- KURUMIBOT/__main__.py
+++ KURUMIBOT/__main__.py
@@ -51,26 +51,26 @@ def get_readable_time(seconds: int) -> str:
 
 
 PM_START_TEXT = """
-Hi {}, my name is {}! 
-I am an Anime themed group management bot. I will use my angel Zafkiel and protect your group. You can find my list of available commands with /help.
+Hi {}, my name is Giorno Giovanna! 
+I am an Anime themed group management bot. I will use my Gold Experience and protect your group. You can find my list of available commands with /help.
 """
 
 HELP_STRINGS = """
-KURUMI is here! 
-I Use My Powers To Help Admins To Manage Their Groups! 
+Giorno is here! 
+I Use My Stand Abilities To Help Admins To Manage Their Groups! 
 *Main* commands available :
  • /help: PM's you this message.
  • /help <module name>: PM's you info about that module.
  • /settings:
    • in PM: will send you your settings for all supported modules.
    • in a group: will redirect you to pm, with all that chat's settings.
-For all command use / [or](https://telegra.ph/file/b1b545eb4877b012eac82.png) ! 
+For all command use / [or](https://telegra.ph/file/bbe217f99ef4585432d51.png) ! 
 """.format(
     dispatcher.bot.first_name, ""
     if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\nKindly use ! for commands if / is not working\n")
 
-TOKISAKI_IMG = "https://telegra.ph/file/99bbcc3b8564804dc2ab0.jpg"
-KURUMI_IMG = "https://telegra.ph/file/e1d87ec2bdac4e3cfe0a4.mp4"
+TOKISAKI_IMG = "https://telegra.ph/file/ad32ae1da39cfdbe89bd6.jpg"
+KURUMI_IMG = "https://telegra.ph/file/ad2700d5cb83fae40784f.mp4"
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
 Click here to donate in [Paypal](https://www.paypal.me/zameeljaz)"""
@@ -191,27 +191,27 @@ def start(update: Update, context: CallbackContext):
                 reply_markup=InlineKeyboardMarkup(
                     [[
                         InlineKeyboardButton(
-                            text="☑️ Add Kurumi Chan to your group",
+                            text="☑️ Add Giorno Chan to your group",
                             url="t.me/{}?startgroup=true".format(
                                 context.bot.username))
                     ],
                      [
                          InlineKeyboardButton(
                              text="🚨 Support 🚨",
-                             url=f"https://t.me/{SUPPORT_CHAT}"),
+                             url=f"https://t.me/the_cult"),
                          InlineKeyboardButton(
-                             text="My luv ❤",
-                             url="https://t.me/A_lonelyPerson")
+                             text="Meme Channel",
+                             url="https://t.me/cultmemebase")
                     ], 
                      [
                          InlineKeyboardButton(
-                             text="🔔 Updates of KURUMI 🔔",
-                             url="https://t.me/hiroiscool")
+                             text="🔔 Off Topic 🔔",
+                             url="https://t.me/WeebsGC")
                     ], 
                      [
                          InlineKeyboardButton(
-                             text="📳 Anime Chat Group 📳",
-                             url="https://t.me/WeebXWorld")
+                             text="📳 Chat Group 📳",
+                             url="https://t.me/The_Cult")
                     ]]))
                      
                     
@@ -219,7 +219,7 @@ def start(update: Update, context: CallbackContext):
                     
     else:
         update.effective_message.reply_video(
-                KURUMI_IMG, caption=f"Zaphkiel!!!!\nI'm here with my Zaphkiel")
+                KURUMI_IMG, caption=f"I, Giorno Giovanna, Have A Dream")
 
 # for test purposes
 def error_callback(update: Update, context: CallbackContext):
@@ -534,7 +534,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "[My Zaphkiel won't let me die!](https://telegra.ph/file/b069f7fbe7eecdb56c36c.mp4)", parse_mode=ParseMode.MARKDOWN) 
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "[Kore ga requiem da!](https://telegra.ph/file/454c6feefed99723d5add.mp4)", parse_mode=ParseMode.MARKDOWN) 
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!")
